9f8c6a7274085a6e3c26efa3c881bac6ba0d21de
Daniel <oliveira.d.c@outlook.com>
2023-12-03 00:41:10 -0300

    remove unnecessary package

--- Main.java
+++ Main.java
@@ -7,7 +7,6 @@ import java.nio.file.Paths;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
 
